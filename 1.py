4336e8f286ffe719041f00a96e890565523820a4
roberthai <hainguyen.sent@gmail.com>
2021-07-22 23:53:54 +0700

    second commit

--- 1.py
+++ 1.py
@@ -1,7 +1,25 @@
 #second file 
+my_list2 = ['red', 'blue', 'black', 'white'] # the list contains all strind values
 
-#first commit 
+print(my_list2)
+#t commit 
 
+color_list1 = ["White", "Yellow"]
+color_list2 = ["Red", "Blue"]
+color_list3 = ["Green", "Black"]
+color_list = color_list1 + color_list2 + color_list3
+print(color_list)
+number = [1,2,3]
+print(number[0]*4)
+print(number*4)
 
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+  
 
 
