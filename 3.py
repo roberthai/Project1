3b926b1c58770e53666261fd5bc2879211f6fcaf
roberthai <63133157+roberthai@users.noreply.github.com>
2021-07-24 22:37:37 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,3 +2,11 @@ color = {"col1" : "Red", "col2" : "Green", "col3" : "Orange" }
 print (color)
 print (color["col1"])
 print (color.get("col1"))
+
+my_dict = {'x':500, 'y':5874, 'z': 560}
+
+key_max = max(my_dict.keys(), key=(lambda k: my_dict[k]))
+key_min = min(my_dict.keys(), key=(lambda k: my_dict[k]))
+
+print('Maximum Value: ',my_dict[key_max])
+print('Minimum Value: ',my_dict[key_min])
